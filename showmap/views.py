a9c3b897a47d1f6191d7ddc972c7d69c513f1dfd
Семён Капустников <s.v.kapustnikov@gmail.com>
2024-08-08 16:28:34 +0300

    index.html отображает локацию

--- showmap/views.py
+++ showmap/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import folium
 
-# Create your views here.
+
+def index(request):
+    template = 'showmap/index.html'
+    usermap = folium.Map(location=(60.178582, 29.969587),
+                         tiles="cartodb positron")
+    folium.Marker(
+        location=[60.178582, 29.969587],
+        tooltip="Click me!",
+        popup="Mt. Hood Meadows",
+        icon=folium.Icon(icon="cloud"),
+    ).add_to(usermap)
+    usermap = usermap._repr_html_()
+    context = {
+        'usermap': usermap}
+    return render(request, template, context)
